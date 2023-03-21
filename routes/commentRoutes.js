5ce81ff393550df4d9c005c9591abcb429ff10ba
Cole Arrington <119268105+colearrington98@users.noreply.github.com>
2023-03-21 15:43:08 -0500

    Authentication and CSS

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const { Comment } = require('../models');
-const router = express.Router();
+const express = require('express'); // Import the express package
+const { Comment } = require('../models'); // Import the Comment model
+const router = express.Router(); // Create a router object
+const isAuthenticated = require('../utils/auth'); // Import the isAuthenticated function
 
 router.post('/post/:id', async (req, res) => {  // Add a comment to a blog post and redirect back to the post
-    await Comment.create({content:req.body.content, user_id:req.session.user.id, blog_id:req.params.id});
-}),
+    await Comment.create({content:req.body.content, user_id:req.session.user.id, blog_id:req.params.id}); // Create a new comment
+}), // Redirect back to the post
 
-router.post('/post/:id', isAuthenticated, async (req, res) => {
-});
+router.post('/post/:id',isAuthenticated, async (req, res) => { // Add a comment to a blog post and redirect back to the post
+}); // Redirect back to the post
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const sequelize = require('./config/connection');
 const express = require('express');
 const routes = require('./controllers');
-const routes = require('./routes')
+const homeRoutes = require('./routes/homeRoutes');
+const userRoutes = require('./routes/userRoutes');
+const blogRoutes = require('./routes/blogRoutes');
+const commentRoutes = require('./routes/commentRoutes');
 const session = require('express-session');
 
 
@@ -21,8 +24,16 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// Set Handlebars.js as the default template engine
+app.use(express.static('public'));
+
 // Turn on routes
 app.use(routes);
+app.use('/', homeRoutes);
+app.use('/users', userRoutes);
+app.use('/blogs', blogRoutes);
+app.use('/comments', commentRoutes);
 // app.use(session(sess));
 
 sequelize.sync()
@@ -32,5 +43,4 @@ sequelize.sync()
   .catch(err => {
     console.log(err);
   });
-
 
