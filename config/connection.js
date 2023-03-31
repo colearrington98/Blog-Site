5c57f67f75a31128314415756b37d0beb2e6928a
Cole Arrington <119268105+colearrington98@users.noreply.github.com>
2023-03-31 11:55:49 -0500

    File restructure and server.js update

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,20 @@
 const Sequalize = require('sequelize'); // import the Sequalize constructor from the sequelize package
 require ('dotenv').config(); // import the dotenv package
 
-const sequelize = new Sequalize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { // create a new instance of the Sequelize constructor
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-});
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+  } else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+      }
+    );
+  }
+ 
 
 module.exports = sequelize; // export the sequelize instance
--- server.js
+++ server.js
@@ -1,50 +1,51 @@
 const path = require('path'); // Import the path package
-const sequelize = require('./config/connection'); // Import the connection to the database
 const express = require('express'); // Import the express package
 const routes = require('./controllers'); // Import the routes from the controllers folder
-const homeRoutes = require('./routes/homeRoutes'); // Import the homeRoutes.js file
-const userRoutes = require('./routes/userRoutes'); // Import the userRoutes.js file
-const blogRoutes = require('./routes/blogRoutes'); // Import the blogRoutes.js file
-const commentRoutes = require('./routes/commentRoutes'); // Import the commentRoutes.js file
 const session = require('express-session'); // Import the express-session package
 const exphbs = require('express-handlebars'); // Import the express-handlebars package
+const helpers = require('./utils/helpers'); // Import the helpers folder
 
 
 
+const sequelize = require('./config/connection'); // Import the connection to the database
+const SequelizeStore = require('connect-session-sequelize')(session.Store); // Import the connect-session-sequelize package
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = exphbs.create({}); // Create a new instance of exphbs
+const hbs = exphbs.create({ helpers }); // Create a new instance of exphbs
+
+// Set up sessions with cookies
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 3600000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // Set Handlebars.js as the default template engine
 app.engine('handlebars', exphbs({ defaultLayout: 'main' })); 
 app.set('view engine', 'handlebars'); // 
 
 
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the public folder
-
-
-// Express.js session middleware
-app.use( 
-  session({
-    secret: 'Super secret secret',
-    resave: false,
-    saveUninitialized: true,
-  }));
-
 // Express.js middleware
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the public folder
 app.use(express.urlencoded({ extended: true }));
-
 
 // Turn on routes
 app.use(routes);
-app.use('/', homeRoutes);
-app.use('/users', userRoutes);
-app.use('/blogs', blogRoutes);
-app.use('/comments', commentRoutes);
-// app.use(session(sess));
+
 
 sequelize.sync() // Sync the models to the database
   .then(() => {
